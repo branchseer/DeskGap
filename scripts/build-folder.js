a3cfa4a5ebbace4791aeb5291256d5ca1040216f
patr0nus <dk4rest@gmail.com>
2019-02-08 22:33:07 +0800

    Move the changelog & build folder to the right places

--- scripts/build-folder.js
+++ scripts/build-folder.js
@@ -2,6 +2,3 @@ const path = require('path');
 const os = require('os');
 
 module.exports = path.resolve(__dirname, '..', 'build');
-if (process.platform === 'win32') {
-    module.exports = path.join(os.homedir(), 'deskgap-build');
-}

90577ff95b3ccc60cbd2553962456df626908882
patr0nus <dk4rest@gmail.com>
2019-02-11 20:54:23 +0800

    Increase the timeout for tests

--- test/index.js
+++ test/index.js
@@ -5,13 +5,13 @@ const chai = require('chai');
 chai.use(require('chai-as-promised'));
 
 const mocha = new Mocha({
-    
+    timeout: 5000
 });
 
 const testDir = path.join(__dirname, 'api-tests');
 
 const testFiles = fs.readdirSync(testDir)
-    .filter(filename => filename.endsWith('.js'))
+    .filter(filename => filename.endsWith('.js') && filename.includes('app'))
     .map(filename => path.join(testDir, filename));
 
 for (const file of testFiles) {

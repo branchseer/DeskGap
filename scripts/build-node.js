147c65dd3c4f255608f012d3a96f804828ba3384
patr0nus <dk4rest@gmail.com>
2019-02-15 23:50:01 +0800

    Allow to build Node in Linux and with single-core cpu

--- scripts/build-node.js
+++ scripts/build-node.js
@@ -7,10 +7,10 @@ const nodeSourcePath = path.resolve(__dirname, '..', 'deps', 'node');
 process.chdir(nodeSourcePath);
 
 let buildCommands;
-if (process.platform === 'darwin') {
+if (process.platform === 'darwin' || process.platform === 'linux') {
     buildCommands = [
         './configure --without-intl --enable-static',
-        `make -j${os.cpus().length - 1}`
+        `make -j${Math.max(os.cpus().length - 1, 1)}`
     ];
 }
 else if (process.platform === 'win32') {

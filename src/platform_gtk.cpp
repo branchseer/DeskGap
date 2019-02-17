290351fbfd425d6456cefc36ffd841c4cec8a6d4
patr0nus <dk4rest@gmail.com>
2019-02-17 17:31:56 +0800

    Make executable buildable

---
+++ src/platform_gtk.cpp
@@ -0,0 +1,19 @@
+#include "platform.h"
+
+void DeskGapPlatform::InitUIThread() {
+
+}
+
+void DeskGapPlatform::InitNodeThread() { }
+
+void DeskGapPlatform::Run() {
+
+}
+
+std::string DeskGapPlatform::PathOfResource(const std::vector<const char*>& paths) {
+    return {};
+}
+
+bool DeskGapPlatform::ResourceExists(const std::vector<const char*>& paths) {
+    return false;
+}

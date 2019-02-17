900cca27ba333e6be0e9c3f9ab08c70caa72d2da
patr0nus <dk4rest@gmail.com>
2019-02-17 22:15:11 +0800

    Implement fs functions for Linux

--- src/platform_gtk.cpp
+++ src/platform_gtk.cpp
@@ -1,4 +1,7 @@
 #include "platform.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 void DeskGapPlatform::InitUIThread() {
 
@@ -7,13 +10,21 @@ void DeskGapPlatform::InitUIThread() {
 void DeskGapPlatform::InitNodeThread() { }
 
 void DeskGapPlatform::Run() {
-
+    
 }
 
 std::string DeskGapPlatform::PathOfResource(const std::vector<const char*>& paths) {
-    return {};
+    static fs::path resourcesFolder = fs::read_symlink("/proc/self/exe").parent_path() / "resources";
+
+    fs::path resourcePath = resourcesFolder;
+    for (const char* component: paths) {
+        resourcePath.append(component);
+    }
+
+    printf("%s\n", resourcePath.c_str());
+    return resourcePath;
 }
 
 bool DeskGapPlatform::ResourceExists(const std::vector<const char*>& paths) {
-    return false;
+    return fs::exists(PathOfResource(paths));
 }

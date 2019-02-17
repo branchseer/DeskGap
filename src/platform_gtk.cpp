15b36065eb41eaa7b6775cb807fe78f84fa78b05
patr0nus <dk4rest@gmail.com>
2019-02-17 22:53:55 +0800

    Add GTK functions in entry program

--- src/platform_gtk.cpp
+++ src/platform_gtk.cpp
@@ -1,16 +1,35 @@
-#include "platform.h"
 #include <filesystem>
+#include <memory>
+#include <gtk/gtk.h>
+
+#include "platform.h"
 
 namespace fs = std::filesystem;
 
+namespace {
+    void deleteGObject(gpointer object) {
+        if (object != nullptr) {
+            g_object_unref(object);
+        }
+    }
+
+    std::unique_ptr<GtkApplication, decltype(&deleteGObject)> gtkApp(nullptr, deleteGObject);
+}
+
 void DeskGapPlatform::InitUIThread() {
+    gtkApp.reset(gtk_application_new(nullptr, G_APPLICATION_FLAGS_NONE));
+    g_application_hold(G_APPLICATION(gtkApp.get()));
+    
+    // Suppress no activate handler warning:
+    g_signal_connect(gtkApp.get(), "activate", G_CALLBACK([](){ }), NULL);
 
 }
 
 void DeskGapPlatform::InitNodeThread() { }
 
 void DeskGapPlatform::Run() {
-    
+    g_application_run(G_APPLICATION(gtkApp.get()), 0, NULL);
+    gtkApp.reset(nullptr);
 }
 
 std::string DeskGapPlatform::PathOfResource(const std::vector<const char*>& paths) {
@@ -21,7 +40,6 @@ std::string DeskGapPlatform::PathOfResource(const std::vector<const char*>& path
         resourcePath.append(component);
     }
 
-    printf("%s\n", resourcePath.c_str());
     return resourcePath;
 }
 

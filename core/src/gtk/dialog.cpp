6ea60020cff5ae4ef6d1f8bef5c7ed962be7e7bf
patr0nus <dk4rest@gmail.com>
2019-02-21 15:17:57 +0800

    gtk implementation for showErrorBox

--- core/src/gtk/dialog.cpp
+++ core/src/gtk/dialog.cpp
@@ -3,7 +3,17 @@
 
 namespace DeskGap {
     void Dialog::ShowErrorBox(const std::string& title, const std::string& content) {
-
+        GtkWidget* dialog = gtk_message_dialog_new(
+            nullptr,
+            GTK_DIALOG_MODAL,
+            GTK_MESSAGE_ERROR,
+            GTK_BUTTONS_CLOSE,
+            title.c_str(),
+            content.c_str()
+        );
+        gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(dialog), "%s", content.c_str());
+        gtk_dialog_run(GTK_DIALOG(dialog));
+        gtk_widget_destroy(dialog);
     }
 
     void Dialog::ShowOpenDialog(

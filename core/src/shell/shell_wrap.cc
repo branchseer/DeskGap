784a189260d39c9eab3604e828aae7402fc8db05
patr0nus <dk4rest@gmail.com>
2019-02-23 15:36:30 +0800

    Shell.OpenExternal

--- core/src/shell/shell_wrap.cc
+++ core/src/shell/shell_wrap.cc
@@ -1,10 +1,14 @@
 #include "shell_wrap.h"
 #include "shell.h"
+#include "../dispatch/ui_dispatch.h"
 
 Napi::Object DeskGap::ShellObject(const Napi::Env& env) {
     Napi::Object shellObject = Napi::Object::New(env);
     shellObject.Set("openExternal",  Napi::Function::New(env, [](const Napi::CallbackInfo& info) {
-        bool success = Shell::OpenExternal(info[0].As<Napi::String>());
+        bool success;
+        UISync(info.Env(), [&success, urlString = info[0].As<Napi::String>()]() {
+        	success = Shell::OpenExternal(urlString);
+        });
         return Napi::Boolean::New(info.Env(), success);
     }));
 

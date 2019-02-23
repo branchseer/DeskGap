28f51169b2060fe5c6c6bf5eaef52537d9919d8f
patr0nus <dk4rest@gmail.com>
2019-02-23 15:36:30 +0800

    Shell.OpenExternal

--- core/src/gtk/shell.cpp
+++ core/src/gtk/shell.cpp
@@ -1,5 +1,7 @@
 #include "../shell/shell.h"
 
+#include <gtk/gtk.h>
+
 bool DeskGap::Shell::OpenExternal(const std::string& urlString) {
-    return true;
+    return gtk_show_uri_on_window(nullptr, urlString.c_str(), GDK_CURRENT_TIME, nullptr);
 }

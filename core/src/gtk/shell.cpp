8ced4b2d606fe88baa3190da3820edd228ec3541
patr0nus <dk4rest@gmail.com>
2019-02-25 08:20:23 -0800

    Support older versions of gtk

--- core/src/gtk/shell.cpp
+++ core/src/gtk/shell.cpp
@@ -3,5 +3,5 @@
 #include <gtk/gtk.h>
 
 bool DeskGap::Shell::OpenExternal(const std::string& urlString) {
-    return gtk_show_uri_on_window(nullptr, urlString.c_str(), GDK_CURRENT_TIME, nullptr);
+	return gtk_show_uri(nullptr, urlString.c_str(), GDK_CURRENT_TIME, nullptr);
 }

faa502fd084513c48ba0c913890b9f36f71510ec
patr0nus <dk4rest@gmail.com>
2019-02-27 16:51:21 +0800

    Basic implementation of win32 ui dispatch

---
+++ core/src/platform_data.h
@@ -0,0 +1,8 @@
+#ifndef platform_data_h
+#define platform_data_h
+
+namespace DeskGap {
+    void* PlatformData();
+}
+
+#endif /* platform_data_h */
--- core/src/win/ui_dispatch_platform.cpp
+++ core/src/win/ui_dispatch_platform.cpp
@@ -1,4 +1,6 @@
+#include <Windows.h>
 #include "../dispatch/ui_dispatch_platform.h"
+#include "../platform_data.h"
 
 namespace {
    std::optional<DeskGap::PlatformException> ExecuteAction(const std::function<void()>& action) {
@@ -8,9 +10,40 @@ namespace {
 }
 
 std::optional<DeskGap::PlatformException> DeskGap::UISyncPlatform(std::function<void()>&& action) {
-   return std::nullopt;
+   static DWORD mainThreadId = *static_cast<DWORD*>(PlatformData());
+
+   std::optional<DeskGap::PlatformException> exception;
+
+   HANDLE actionCompleted = CreateEventEx(nullptr, nullptr, 0, EVENT_ALL_ACCESS);
+   HANDLE events[1] = { actionCompleted };
+   DWORD handleCount = ARRAYSIZE(events);
+   DWORD handleIndex = 0;
+
+   PostThreadMessageW(
+      mainThreadId,
+      WM_APP, 0,
+      reinterpret_cast<LPARAM>(new std::function<void()>([&]() {
+         exception = ExecuteAction(action);
+         SetEvent(actionCompleted);
+      }))
+   );
+
+   CoWaitForMultipleHandles(0, INFINITE, handleCount, events, &handleIndex);
+   CloseHandle(actionCompleted);
+
+   return exception;
 }
 
 void DeskGap::UIASyncPlatform(std::function<void()>&& action, std::function<void(std::optional<PlatformException>&&)> callback) {
-
+   static DWORD mainThreadId = *static_cast<DWORD*>(PlatformData());
+   PostThreadMessageW(
+      mainThreadId,
+      WM_APP, 0,
+      reinterpret_cast<LPARAM>(new std::function<void()>([
+         action = std::move(action),
+         callback = std::move(callback)
+      ]() {
+         callback(ExecuteAction(action));
+      }))
+   );
 }

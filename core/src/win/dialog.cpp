99cbb65ffb08ec465750fb9be4c3b90863f49996
patr0nus <dk4rest@gmail.com>
2019-02-28 15:56:19 +0800

    Implement Dialog.ShowErrorBox

--- core/src/win/dialog.cpp
+++ core/src/win/dialog.cpp
@@ -1,13 +1,30 @@
+#include <Windows.h>
+#include <Commctrl.h>
+
 #include "./BrowserWindow_impl.h"
 #include "../dialog/dialog.h"
 
+#include "./util/wstring_utf8.h"
+
+#pragma comment(lib, "Comctl32")
 
 using CommonFileDialogOptions = DeskGap::Dialog::CommonFileDialogOptions;
 using FileFilter = CommonFileDialogOptions::FileFilter;
 
 namespace DeskGap {
     void Dialog::ShowErrorBox(const std::string& title, const std::string& content) {
-        
+        TASKDIALOGCONFIG config { };
+        config.cbSize = sizeof(config);
+        config.hInstance = GetModuleHandleW(NULL);
+        config.dwFlags = TDF_SIZE_TO_CONTENT;
+        config.pszWindowTitle = L"Error";
+        config.pszMainIcon = TD_ERROR_ICON;
+
+        std::wstring wtitle = UTF8ToWString(title.c_str());
+        std::wstring wcontent = UTF8ToWString(content.c_str());
+        config.pszMainInstruction = wtitle.c_str();
+        config.pszContent =wcontent.c_str();
+        TaskDialogIndirect(&config, nullptr, nullptr, nullptr);
     }
 
     struct Dialog::Impl {

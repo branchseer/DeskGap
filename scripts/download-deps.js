440c1d0dfacbc940208d337722ed4e216b12b06d
patr0nus <dk4rest@gmail.com>
2019-02-28 23:44:59 +0800

    Remove WinForms UWP WebView downloading

--- scripts/download-deps.js
+++ scripts/download-deps.js
@@ -11,25 +11,6 @@ process.chdir(depsFolder);
 
 process.on('unhandledRejection', e => { throw e });
 (async () => {
-    if (process.platform === 'win32') {
-        const nugetPackage = 'webview_nuget.zip';
-
-        await downloadFile(
-            'https://www.nuget.org/api/v2/package/Microsoft.Toolkit.Forms.UI.Controls.WebView/5.1.0',
-            nugetPackage
-        );
-
-        await decompress(nugetPackage, '.', {
-            filter: file => file.path === 'lib/net462/Microsoft.Toolkit.Forms.UI.Controls.WebView.dll',
-            map: file => {
-                file.path = 'Microsoft.Toolkit.Forms.UI.Controls.WebView.dll'
-                return file;
-            }
-        });
-
-        fs.unlinkSync(nugetPackage);
-    }
-
     const nodeSourceURL = `https://nodejs.org/download/release/${nodeVersion}/node-${nodeVersion}.tar.gz`;
     const nodeSourcePackage = 'node.tar.gz';
     await downloadFile(nodeSourceURL, nodeSourcePackage);

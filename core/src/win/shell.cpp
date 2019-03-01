82430671d460777ae9592bb9c88a37408826db58
patr0nus <dk4rest@gmail.com>
2019-03-01 16:18:04 +0800

    Shell.OpenExternal

--- core/src/win/shell.cpp
+++ core/src/win/shell.cpp
@@ -1,5 +1,12 @@
 #include "../shell/shell.h"
+#include "./util/wstring_utf8.h"
 
 bool DeskGap::Shell::OpenExternal(const std::string& urlString) {
-    return false;
+	std::wstring wUrlString = UTF8ToWString(urlString.c_str());
+    return ShellExecuteW(
+    	nullptr, L"open",
+    	wUrlString.c_str(),
+    	nullptr, nullptr,
+    	SW_SHOWNORMAL
+    ) > (HINSTANCE)32;
 }

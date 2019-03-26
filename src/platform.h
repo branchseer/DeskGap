4ae5ac18a908e64b2f9b0b6c75cde02197ce0bd2
patr0nus <dk4rest@gmail.com>
2019-03-26 09:00:36 +0800

    Remove the unnecessary dllexport

--- src/platform.h
+++ src/platform.h
@@ -4,19 +4,13 @@
 #include <string>
 #include <vector>
 
-#if _WIN32
-    #define PLATFORM_EXPORT __declspec(dllexport)
-#else
-    #define PLATFORM_EXPORT
-#endif
-
 namespace DeskGapPlatform {
-    PLATFORM_EXPORT void* InitUIThread();
-    PLATFORM_EXPORT void InitNodeThread();
-    PLATFORM_EXPORT void Run();
+    void* InitUIThread();
+    void InitNodeThread();
+    void Run();
 
-    PLATFORM_EXPORT std::string PathOfResource(const std::vector<const char*>& name);
-    PLATFORM_EXPORT bool ResourceExists(const std::vector<const char*>& name);
+    std::string PathOfResource(const std::vector<const char*>& name);
+    bool ResourceExists(const std::vector<const char*>& name);
 }
 
 #endif

d9d48b2a6a838f919f37f50b63e871145f2d1b65
patr0nus <dk4rest@gmail.com>
2019-04-08 20:20:33 +0800

    Add tests for webView.executeJavaScript(code)

--- test/api-tests/webview.js
+++ test/api-tests/webview.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, messageNode } = require('deskgap');
 const { expect } = require('chai');
 const { createLocalServer } = require('../utils');
+const { once } = require('events');
 const path = require('path');
 
 describe('BrowserWindow#webView', () => {
@@ -28,7 +29,7 @@ describe('BrowserWindow#webView', () => {
     });
 
     describe('webView.loadURL(url)', () => {
-        if (process.platform !== 'win32') it('loads the page by requesting the url', async () => {
+        it('loads the page by requesting the url', async () => {
             let resolve;
             let requested = false;
             const server = await createLocalServer({
@@ -37,16 +38,49 @@ describe('BrowserWindow#webView', () => {
                     resolve();
                 }
             });
+            
             win.webView.loadURL(server.url + '/index.html');
             await new Promise(r => resolve = r);
             expect(requested).to.equal(true);
+            server.close();
         });
     });
 
     describe('webView.loadFile(path)', () => {
-        it('loads the give file', (done) => {
+        it('loads the given file', (done) => {
             win.webView.loadFile(path.resolve(__dirname, '..', 'fixtures', 'files', 'web-view-load-file.html'));
             messageNode.once('hello-web-view-load-file', () => done());
         })
+    });
+
+    describe('webView.executeJavaScript(code)', () => {
+        beforeEach(async () => {
+            win.loadFile(path.resolve(__dirname, '..', 'fixtures', 'files', 'blank.html'));
+            await once(win.webView, 'did-finish-load');
+        });
+        it('returns a promise of the evaluated value', async () => {
+            const result = await win.webView.executeJavaScript("{ answer: 40+2 }");
+            expect(result).to.deep.equal({ answer: 42 });
+        });
+
+        it('returns a promise that resolves to the result of the evaluated promise', async () => {
+            const result = await win.webView.executeJavaScript("Promise.resolve({ answer: 40+2 })");
+            expect(result).to.deep.equal({ answer: 42 });
+        });
+
+        it('returns a promise that rejects when there is a syntax error', async () => {
+            const resultPromise = win.webView.executeJavaScript("nonsense");
+            await expect(resultPromise).to.eventually.rejected;
+        });
+
+        it('returns a promise that rejects when the code throws an error synchronously', async () => {
+            const resultPromise = win.webView.executeJavaScript("(function(){ throw new Error(); })()");
+            await expect(resultPromise).to.eventually.rejected;
+        });
+
+        it('returns a promise that rejects when the code is evaluated to a promise that rejects', async () => {
+            const resultPromise = win.webView.executeJavaScript("Promise.reject(new Error())");
+            await expect(resultPromise).to.eventually.rejected;
+        });
     });
 });

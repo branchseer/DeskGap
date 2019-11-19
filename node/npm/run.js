50b2eed77ab3c41b48c338139ccead84f9875f51
patr0nus <dk4rest@gmail.com>
2019-11-19 16:56:42 +0800

    Separate the runDeskGap function

---
+++ node/npm/run.js
@@ -0,0 +1,34 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+module.exports = (distPath, entryPath, args) => {
+    let executablePath;
+    if (process.platform === 'darwin') {
+        executablePath = path.join(distPath, 'DeskGap.app/Contents/MacOS/DeskGap');
+    }
+    else if (process.platform === 'win32') {
+        executablePath = path.join(distPath, 'DeskGap/DeskGap.exe');
+    }
+    else if (process.platform === 'linux') {
+        executablePath = path.join(distPath, 'DeskGap/DeskGap');
+    }
+
+    const deskgapProcess = spawn(executablePath, args, {
+        stdio: 'inherit',
+        windowsHide: false,
+        env: {
+            ...process.env,
+            'DESKGAP_ENTRY': path.resolve(entryPath)
+        }
+    });
+
+    for (const signal of ['SIGINT', 'SIGTERM']) {
+        process.on(signal, () => {
+            if (!deskgapProcess.killed) {
+                deskgapProcess.kill(signal);
+            }
+        })
+    }
+
+    deskgapProcess.once('close', (code) => process.exit(code));
+};

98f641635ab77adbc0102b8d265a974fc7e678e5
patr0nus <dk4rest@gmail.com>
2019-11-25 00:37:47 +0800

    Update

--- node/test/start.js
+++ node/test/start.js
@@ -4,10 +4,10 @@ const path = require('path');
 const distPath = process.argv[2];
 
 if (distPath == null) {
-    console.error("Missing [deskgap-dist-path]");
-    console.error('Usage: node start.js [deskgap-dist-path]');
+    console.error("Missing [deskgap-dist-path] [args...]");
+    console.error('Usage: node start.js [deskgap-dist-path] [args...]');
     process.exit(1);
 }
 else {
-    runDeskGap(distPath, __dirname, []);
+    runDeskGap(distPath, __dirname, process.argv.slice(3));
 }

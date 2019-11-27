bb060e0cddf115b6a2b3c89def0e18316ebda4c6
patr0nus <dk4rest@gmail.com>
2019-11-27 17:37:06 +0800

    Add event_callbacks.on_ready to dg_app_run

--- lib/src/wrappers/app_c.cc
+++ lib/src/wrappers/app_c.cc
@@ -12,12 +12,14 @@ extern "C" {
 using DeskGap::App;
 void dg_app_run(dg_app_event_callbacks event_callbacks, const void* user_data) {
     App::EventCallbacks callbacks;
-    if (event_callbacks.before_quit != nullptr) {
-        callbacks.onReady = [user_data, event_callbacks]() {
-            event_callbacks.on_ready(user_data);
+    if (event_callbacks.on_ready != nullptr) {
+        callbacks.onReady = [user_data, on_ready { event_callbacks.on_ready }]() {
+            on_ready(user_data);
         };
-        callbacks.beforeQuit = [user_data, event_callbacks]() {
-            event_callbacks.before_quit(user_data);
+    }
+    if (event_callbacks.before_quit != nullptr) {
+        callbacks.beforeQuit = [user_data, before_quit { event_callbacks.before_quit }]() {
+            before_quit(user_data);
         };
     }
     DeskGap::App::Run(std::move(callbacks));

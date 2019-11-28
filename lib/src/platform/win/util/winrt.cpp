8a9e83d918f9b7a9192ea4184db597662e4bb855
patr0nus <dk4rest@gmail.com>
2019-11-28 14:45:21 +0800

    Simplify the winrt load routine

--- lib/src/platform/win/util/winrt.cpp
+++ lib/src/platform/win/util/winrt.cpp
@@ -1,28 +1,15 @@
-#include "winrt.h"
+#include <mutex>
 
+#include "winrt.h"
 #include "win32_check.h"
-#include <filesystem>
 #include "winrt_webview.hpp"
 
+static std::once_flag winrtDLLLoadOnceFlag;
+static HMODULE winrtDLLModule;
+
 HMODULE DeskGap::GetWinRTDLLModule() {
-    namespace fs = std::filesystem;
-
-    static bool firstRun = true;
-    static HMODULE winrtDLLModule;
-    if (firstRun) {
-        firstRun = false;
-
-        HMODULE currentDLLModule;
-        check(GetModuleHandleExW(
-            GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-            (LPCWSTR)&GetWinRTDLLModule, &currentDLLModule
-        ));
-
-        wchar_t currentDLLPath[MAX_PATH];
-        check(GetModuleFileNameW(currentDLLModule, currentDLLPath, sizeof(currentDLLPath)));
-        fs::path winrtDllPath = fs::path(currentDLLPath).parent_path() / L"deskgap_winrt.dll";
-
-        winrtDLLModule = LoadLibraryW(winrtDllPath.c_str());
-    }
+    std::call_once(winrtDLLLoadOnceFlag, []() {
+        winrtDLLModule = LoadLibraryW(L"deskgap_winrt.dll");
+    });
     return winrtDLLModule;
 }

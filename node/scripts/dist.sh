4f0c36d0304663e575a97a81abb63c3ad0477e3e
patr0nus <dk4rest@gmail.com>
2019-11-28 14:35:52 +0800

    Tidy the dist.sh script

--- node/scripts/dist.sh
+++ node/scripts/dist.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd $DIR/..
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    CMakeGenerator="Unix Makefiles";
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    CMakeGenerator="Unix Makefiles";
-else
-    CMakeGenerator="Visual Studio 16 2019";
-fi
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+deskGapNodeDir=scriptDir/..
 
 rm -rf dist_build
-cmake -G "$CMakeGenerator" -DCMAKE_BUILD_TYPE=Release -S . -B dist_build
-cmake --build dist_build --config Release
+
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+  cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -S "$deskGapNodeDir" -B dist_build
+  cmake --build dist_build
+else
+  cmake -G "Visual Studio 16 2019" -S "$deskGapNodeDir" -B dist_build
+  cmake --build dist_build --config Release
+fi
 
 cd dist_build
 ls

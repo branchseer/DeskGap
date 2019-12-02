213e603a13f386f6eba2f287f54f662175401f16
patr0nus <dk4rest@gmail.com>
2019-12-02 21:03:12 +0800

    CI: cd into the npm_test directory[skip travis]

--- node/scripts/test-npm.sh
+++ node/scripts/test-npm.sh
@@ -11,7 +11,10 @@ fi
 
 rm -rf ./npm_test && cp -r $scriptDir/../npm/test ./npm_test
 ls npm_test
-npm --prefix npm_test install
-npm --prefix npm_test install "$npmTGZFile"
-npm --prefix npm_test run test-js
-npm --prefix npm_test run test-ts
+cd npm_test
+echo "Installing denpendencies of npm_test"
+npm install
+echo "Installing DeskGap to npm_test"
+npm install "$npmTGZFile"
+npm run test-js
+npm run test-ts

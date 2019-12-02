638682c7d3764ac4d91f19c9ec48342ff4dbbeda
patr0nus <dk4rest@gmail.com>
2019-12-02 19:05:22 +0800

    CI: Remove the file existing test in test-npm.sh (not working on Windows)

--- node/scripts/test-npm.sh
+++ node/scripts/test-npm.sh
@@ -4,14 +4,10 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $scriptDir$
 
 npmTGZFile=$1
-if [ ! -f "$npmTGZFile" ]; then
-    echo "The npmTGZFile file does not exist: $npmTGZFile"
-    exit 1
-fi
 
 rm -rf ./npm_test && cp -r $scriptDir/../npm/test ./npm_test
 ls npm_test
 npm --prefix npm_test install
-npm --prefix npm_test install "$npmTGZFile"
+npm --prefix npm_test install '$npmTGZFile'
 npm --prefix npm_test run test-js
 npm --prefix npm_test run test-ts

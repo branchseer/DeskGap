fbb8c768be6cc60b58a57957219eb2fc8b5a82ab
patr0nus <dk4rest@gmail.com>
2019-12-02 21:21:42 +0800

    CI: assume the npm.tgz in pwd in test-npm.sh [skip travis]

--- node/scripts/test-npm.sh
+++ node/scripts/test-npm.sh
@@ -3,18 +3,11 @@ set -e
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $scriptDir$
 
-npmTGZFile=$1
-if [ ! -f "$npmTGZFile" ]; then
-    echo "The npmTGZFile file does not exist: $npmTGZFile"
-    exit 1
-fi
-
 rm -rf ./npm_test && cp -r $scriptDir/../npm/test ./npm_test
-ls npm_test
-cat npm_test/package.json
+cd npm_test
 echo "Installing denpendencies of npm_test"
-npm --prefix npm_test install
+npm install
 echo "Installing DeskGap to npm_test"
-npm --prefix npm_test install "$npmTGZFile"
-npm --prefix npm_test run test-js
-npm --prefix npm_test run test-ts
+npm npm_test install ../npm.tgz
+npm npm_test run test-js
+npm npm_test run test-ts

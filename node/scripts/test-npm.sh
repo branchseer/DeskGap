224ac61ceebf98ad6eac036f1a3f23e2ca0b077a
patr0nus <dk4rest@gmail.com>
2019-12-02 20:28:04 +0800

    CI: double-quote variable [skip travis]

--- node/scripts/test-npm.sh
+++ node/scripts/test-npm.sh
@@ -8,6 +8,6 @@ npmTGZFile=$1
 rm -rf ./npm_test && cp -r $scriptDir/../npm/test ./npm_test
 ls npm_test
 npm --prefix npm_test install
-npm --prefix npm_test install '$npmTGZFile'
+npm --prefix npm_test install "$npmTGZFile"
 npm --prefix npm_test run test-js
 npm --prefix npm_test run test-ts

2dc1c74b39e91d88bc25cacd191c1ca4cdd6454e
patr0nus <dk4rest@gmail.com>
2019-12-04 11:27:39 +0800

    Check DeskGap version in npm test [skip travis]

--- node/npm/test/index-ts.ts
+++ node/npm/test/index-ts.ts
@@ -1,6 +1,9 @@
 import { app } from 'deskgap';
+import assert = require('assert');
+import fs = require('fs');
 
 app.once('ready', () => {
-	console.log('index-ts.ts: DeskGap app ready. Exiting...');
+	console.log('index-ts.ts: DeskGap app ready.');
+	assert.strictEqual(fs.readFileSync(process.env['DESKGAP_NPM_TEST_VERSION_FILE'], 'utf8'), process.versions.deskgap)
 	app.exit();
 });

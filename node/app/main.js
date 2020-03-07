352488fd184d515afad416a707e06e89fc63d5ce
patr0nus <dk4rest@gmail.com>
2020-03-07 19:02:55 +0800

    Rewrite examples

--- node/app/main.js
+++ node/app/main.js
@@ -1,30 +1,33 @@
-const { app, BrowserWindow, MenuItem, Menu, ipcMain, systemPreferences, dialog } = require('deskgap');
-
-ipcMain.on('hello-to-node', (e, message) => {
-    e.sender.send('hello-from-node', "Hello, " + message);
-});
-
-let mainWindow;
+const { app, BrowserWindow, dialog } = require('deskgap');
 
 app.once('ready', () => {
-    mainWindow = new BrowserWindow({
+    const mainWindow = new BrowserWindow({
         show: false,
         width: 800, height: 600,
     }).once('ready-to-show', () => {
+        
+        for (const eventName of ['blur', 'focus']) {
+            mainWindow.on(eventName, () => { 
+                mainWindow.webView.getService('').send('windowEvent', eventName);
+            })
+        }
+
         mainWindow.show();
+    });
+
+    mainWindow.webView.publishServices({
+        "dialog": dialog,
+        "browserWindow": mainWindow,
+        "": {
+            execute(code) {
+                return (new Function('browserWindow', code))(mainWindow);
+            }
+        }
     });
 
     if (process.platform !== 'win32') {
         mainWindow.webView.setDevToolsEnabled(true);
     }
 
-    for (const eventName of ['blur', 'focus']) {
-        mainWindow.on(eventName, () => { mainWindow.webView.send('window-' + eventName) })
-    }
-
     mainWindow.loadFile("app.html");
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
 });
